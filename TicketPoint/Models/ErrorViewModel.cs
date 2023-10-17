f10f8c4ee4c909968161081771a2be6029e5fcca
Mohsin <Mohsin@MOHSIN>
2023-10-17 15:18:48 +0530

    Add project files.

---
+++ TicketPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketPoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
